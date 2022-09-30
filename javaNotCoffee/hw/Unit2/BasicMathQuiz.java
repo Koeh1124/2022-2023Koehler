ae1eb309cbc4a7461e5d9d26b607486e43d6cc78
Stormtroopes <keenan.koehler@evsck12.com>
2022-09-30 14:03:54 -0500

    finished math quiz

--- javaNotCoffee/hw/Unit2/BasicMathQuiz.java
+++ javaNotCoffee/hw/Unit2/BasicMathQuiz.java
@@ -1,49 +1,53 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 import java.lang.Math;
 
 public class BasicMathQuiz {
     public static void main(String[] args) {
-        Scanner ui = new Scanner(System.in);
         Random rand = new Random();
-        int points = 0;
+        Scanner ui = new Scanner(System.in);
+        double points=0;
+        int numOQuestions=0;
+        //        (cast to int)(random decimal * 100)  0-100
+        
         double answer=0.0;
         double input=0.0;
-
-        //loop 10 times
-        for(int i=0;i<10;i++){
-            //generate a math expression
-            int num1 = (int)(Math.random()*100); // random decimal(0-1)*100 casted to int, bassically 0-100
-            int num2 = rand.nextInt(100)+1; // 1-100
-            int signNum = rand.nextInt(4); //0-3
-            if(signNum==0){
-                System.out.printf("what is %d / %d: ",num1,num2);
-                answer = (double)num1/num2;
-            }
-            else if(signNum==1){
-                System.out.printf("what is %d * %d: ",num1,num2);
-                answer = (double)num1*num2;
-            }
-            else if(signNum==2){
-                System.out.printf("what is %d + %d: ",num1,num2);
-                answer = (double)num1+num2;
-            }
-            else{
-                System.out.printf("what is %d - %d: ",num1,num2);
-                answer = (double)num1-num2;
-            }
-            input = ui.nextDouble();
-            if(input==answer){
-                System.out.println("Good job!");
-                points++;
-            }
-            else{
-                System.out.printf("Stop being stupid! The answer was %f\n",answer);
-            }
+        //loop the program 10 times
+        while(points/numOQuestions<.8||numOQuestions<10){
+        //generate a math expression
+        int num1 = (int)(Math.random()*100);
+        int num2 = rand.nextInt(100)+1;    //1-100
+        int signNumber = rand.nextInt(4);   //0-3
+        if(signNumber==0){
+            System.out.printf("What is %d / %d? ",num1,num2);
+            answer = (double)num1/num2;
         }
+        else if(signNumber==1){
+            System.out.printf("What is %d * %d? ",num1,num2);
+            answer = (double)num1*num2;
+        }
+        else if(signNumber==2){
+            System.out.printf("What is %d + %d? ",num1,num2);
+            answer = (double)num1+num2;
+            }
+        else{
+            System.out.printf("What is %d - %d? ",num1,num2);
+            answer = (double)num1-num2;
+            }
+        input = ui.nextDouble();
         //check to see if the user answered the expression correctly
-        
+        if(input==answer){
+            System.out.println("Great job! That's correct!");
+            points++;
+        }
+        else{
+            System.out.printf("Ooo so close, the answer was %f\n",answer);
+        }
+        numOQuestions++;
+        }   
+            
         //output their points
-        System.out.printf("Your total points were %d/10",points);
+        System.out.printf("Out of %d questions you got %d correct",numOQuestions,(int)points);
+            
+        }
     }
-}
