c1f96636c77a1e829f2f73ae7fb73ba29f488d91
Koeh1124 <keenan.koehler@evsck12.com>
2022-11-17 19:45:21 -0600

    puching changes from home

--- passwordMangager/TestingUi.java
+++ passwordMangager/TestingUi.java
@@ -1,13 +1,5 @@
 public class TestingUi {
     public static void main(String[] args) {
-        Password pass = new Password("bruh");
-        Account acc1 = new Account("Netflix", "MyUser");
-        Account acc2 = new Account("Hulu", "MyOtherUser", pass);
-        Account acc3 = new Account("Disney+", "User3", "H8%bvnj7");
-        System.out.println(acc1.toString());
-        System.out.println(acc2.toString());
-        System.out.println(acc3.toString());
-        Password pass2 = acc3.getPassword();
-        System.out.println(pass2.isStrong());
+
     }
 }
